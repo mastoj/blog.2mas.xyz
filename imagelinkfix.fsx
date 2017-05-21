3c4aabc31950ab4268d77226c16d1b673df7e4dd
Tomas Jansson <tomas.jansson@unacast.com>
2017-05-22 00:00:37 +0200

    almost there

--- imagelinkfix.fsx
+++ imagelinkfix.fsx
@@ -2,6 +2,10 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 
+type Link = 
+    | Inline of string
+    | Reference of string * string
+
 let split (splitOn:char) (str:String) = 
     str.Split(splitOn) |> List.ofArray
 
@@ -14,9 +18,9 @@ let readFile file =
 let inlinePattern = """!\[.+\]\((.+)\)"""
 let referencePattern = """!\[.+\]\[(\d+)\]"""
 
-let referenceLinkPattern refNo = sprintf "\\[%s\\]\\:\\s(.+)$" refNo
+let referenceLinkPattern refNo = sprintf "\\[%s\\]\\:\\s([^\\s]+)(.*)" refNo
 
-let matchLinks pattern content = 
+let matchLinks (groupHandler) pattern content = 
     let regex = new Regex(pattern)
     let matches = regex.Matches(content)
     let en = matches.GetEnumerator()
@@ -24,7 +28,7 @@ let matchLinks pattern content =
     let rec loop() = seq {
         if en.MoveNext() 
         then 
-            yield (en.Current :?> Match).Groups.Item(1).ToString()
+            yield (en.Current :?> Match).Groups |> groupHandler //.Item(1).ToString()
             yield! loop() }
     loop()
 
@@ -33,11 +37,12 @@ let findLinks strategy (file, content) =
     |> Seq.toList
     |> List.map (fun m -> file, m)
 
-let matchInlineLinks = matchLinks inlinePattern
+let matchInlineLinks = matchLinks (fun (grp:GroupCollection) -> grp.Item(1).ToString() |> Inline) inlinePattern
 
 let matchFooterLinks content = 
-    matchLinks referencePattern content 
-    |> Seq.map (fun refStr -> matchLinks (referenceLinkPattern refStr) content)
+    matchLinks (fun (grp:GroupCollection) -> grp.Item(1).ToString() |> Inline) referencePattern content 
+    |> Seq.map (fun (Inline refStr) -> matchLinks (fun (grp:GroupCollection) -> Reference (grp.Item(1).ToString(), grp.Item(2).ToString())) (referenceLinkPattern refStr) content)
+//    |> Seq.map (fun x -> printfn "=====> %A" x; x)
     |> Seq.collect id
 
 let findImageLinks input = 
@@ -63,21 +68,37 @@ let downloadFile (url:string) (fileName:string) =
         printfn "Failed to download: %s" url
         ()
 
-let updateFile (file, (oldLink:string), _, (newLink:string)) =
+let updateFile (file, (oldLink:Link), (newLink:string)) =
+    let link = 
+        match oldLink with
+        | Inline str -> str
+        | Reference (str, _) -> str
     let text = File.ReadAllText(file)
-    let transformed = text.Replace(oldLink, newLink)
+    let transformed = text.Replace(link, newLink)
 //    let transformed = seq { for line in File.ReadLines(inFile) -> Regex.Replace(line, "\t",",") }
     File.WriteAllText(file, transformed) 
+
+let mapToNewName link = 
+    let extractName = split '/' >> List.last >> split '?' >> List.head
+    match link with
+    | Inline str
+    | Reference (str, _) -> str |> extractName
+    // | Reference (str, altText) -> 
+    //     printfn "===> %s" str
+    //     printfn "===> %s" altText
+    //     sprintf "%s" (str |> extractName)
+    |> sprintf "{{ site.url }}/assets/images/migrated/%s"
 
 let result = 
     getFiles "_posts"
     |> Seq.map (readFile >> findImageLinks)
     |> Seq.collect id
     |> Seq.toList
-    |> List.map (fun (f, l) -> (f,l, (l |> split '/' |> List.last |> split '?' |> List.head)))
-    |> List.map 
-        (fun (f, l, n) ->
-            f, l, n, (sprintf "{{ site.url }}/assets/images/migrated/%s" n))
+    |> List.map (fun (f,l) -> f,l, (mapToNewName l))
+//    |> List.map (fun (f, l) -> (f,l, (l |> split '/' |> List.last |> split '?' |> List.head)))
+    // |> List.map 
+    //     (fun (f, l, n) ->
+    //         f, l, n, (sprintf "{{ site.url }}/assets/images/migrated/%s" n))
 
 result //|> (printfn "====> %A")
     |> List.iter updateFile
